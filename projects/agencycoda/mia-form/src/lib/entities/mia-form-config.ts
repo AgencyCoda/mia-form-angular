e9d9f442c89f362e2a0fa721cec37d8672e41ed5
Álvaro Enuel Serein <alvaro@tots.agency>
2024-10-22 15:04:34 -0300

    errorMessages removed from MiaFormConfig

--- projects/agencycoda/mia-form/src/lib/entities/mia-form-config.ts
+++ projects/agencycoda/mia-form/src/lib/entities/mia-form-config.ts
@@ -4,7 +4,5 @@ import { MiaField } from "./mia-field";
 export class MiaFormConfig {
     fields: Array<MiaField> = [];
     service!: MiaBaseCrudHttpService<any>;
-    /** @deprecated Does nothing anymore. Remove all usages until this variable can be deleted */
-    errorMessages?: Array<{key: string, message: string}> = [];
     hasSubmit = true;
 }
